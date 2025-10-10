eadf5ceb46c86e2d0e9edfd897a8ad4806cccc12
Jihyeon1520 <hyeon_0225@naver.com>
2025-10-10 23:51:56 +0900

    [FEAT] 키워드를 통한 주제 추천 기능 구현
    agenda_api.py에 키워드를 통한 주제 추천 기능 추가

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ import google.generativeai as genai
 # 내부 모듈
 from app.core.config import settings
 from app.database import Base, engine
-from app.routers import keyword_api, letter_api
+from app.routers import keyword_api, letter_api, ajenda_api
 
 # Gemini API 키 설정
 genai.configure(api_key=settings.GEMINI_API_KEY)
@@ -18,6 +18,7 @@ app = FastAPI(title="GAMO AI Keyword API")
 # 라우터 등록
 app.include_router(keyword_api.router, prefix="/api", tags=["Keywords"])
 app.include_router(letter_api.router, prefix="/api", tags=["Letters"])
+app.include_router(ajenda_api.router, prefix="/api", tags=["Agendas"])
 
 @app.get("/")
 def root():
